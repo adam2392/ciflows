424a6826736c03ab99df1011ab52c5336327d589
Adam Li <adam2392@gmail.com>
2024-10-31 17:05:53 -0400

    8 layer
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- scripts/01_train_basic_models.py
+++ scripts/01_train_basic_models.py
@@ -437,7 +437,7 @@ if __name__ == "__main__":
         bij_model = get_bij_model(n_chs=n_chs, latent_size=latent_size)
         initialize_flow(bij_model)
 
-        debug = True
+        debug = False
         fast_dev = False
         max_epochs = 2000
         if debug:
