c3b7357683fe461218ff4b3f3d6d85809ad7eede
Adam Li <adam2392@gmail.com>
2024-12-12 10:13:17 -0800

    Run dpp
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- scripts/causalfreeformflow/submit-python-jobs.sh
+++ scripts/causalfreeformflow/submit-python-jobs.sh
@@ -6,8 +6,6 @@ SCRIPT_NAME="02_train_fff_ddp_celeba.py"
 
 LOG_DIR="/home/adam2392/projects/logs/"
 
-# Number of GPUs available
-NUM_GPUS=3
 
 # Change to the directory containing the script
 # cd "$SCRIPT_DIR"
@@ -22,7 +20,10 @@ TRAINING_SEED=0
 
 # Calculate the GPU index to use for this job
 # GPU_INDEX=$(((({TRAINING_SEED[$i]}) % $NUM_GPUS) + 1))
-# GPU_INDEX=2
+GPU_INDEX=2
+
+# Number of GPUs available
+NUM_GPUS=3
 
 # Specify the GPUs to use
 GPU_INDICES="5,6,7"  # Adjust this as per available GPUs and your requirement
@@ -33,12 +34,13 @@ GPU_INDICES="5,6,7"  # Adjust this as per available GPUs and your requirement
 export CUDA_VISIBLE_DEVICES=$GPU_INDICES
 
 # Construct the command to run the Python script with the current training seed
-# CMD="python3 $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
-CMD="torchrun --nproc_per_node=$NUM_GPUS $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
+CMD="python3 $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
+# CMD="torchrun --nproc_per_node=$NUM_GPUS $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
 
 # Optionally, you can use a job scheduler like `nohup` to run the command in the background
 # or `&` to run the command in the background
 LOG_FILE="celeba_fff_resnet_batch512_latentdim48_v1_${SCRIPT_NAME}_multigpu.log"
+LOG_FILE="celeba_fff_resnet_batch512_latentdim48_v1_${SCRIPT_NAME}_gpuindex_${GPU_INDEX}.log"
 nohup $CMD > $LOG_FILE 2>&1 &
 
 echo $TRAINING_SEED
