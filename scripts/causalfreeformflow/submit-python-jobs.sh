d80365490c7af06ccf3f84042d3be0a66df9a337
Adam Li <adam2392@gmail.com>
2024-12-17 20:23:07 -0500

    Add fff
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- scripts/causalfreeformflow/submit-python-jobs.sh
+++ scripts/causalfreeformflow/submit-python-jobs.sh
@@ -39,7 +39,7 @@ CMD="python3 $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
 # Optionally, you can use a job scheduler like `nohup` to run the command in the background
 # or `&` to run the command in the background
 CMD="torchrun --nproc_per_node=$NUM_GPUS $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
-LOG_FILE="celeba_fff_resnet_batch512_gradaccum_latentdim48_v1_${SCRIPT_NAME}_multigpu.log"
+LOG_FILE="celeba_fff_resnet_batch512_gradaccum_latentdim48_beta100_v1_${SCRIPT_NAME}_multigpu.log"
 # LOG_FILE="celeba_fff_resnet_batch512_gradaccum_latentdim48_v1_${SCRIPT_NAME}_gpuindex_${GPU_INDEX}.log"
 nohup $CMD > $LOG_FILE 2>&1 &
 
