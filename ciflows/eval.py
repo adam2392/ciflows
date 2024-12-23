62faf7621e3473477075e86c316a0f62349753c5
Adam Li <adam2392@gmail.com>
2024-12-22 22:25:00 -0500

    Running encodings
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- ciflows/eval.py
+++ ciflows/eval.py
@@ -5,12 +5,12 @@ def load_model(model, model_path, device, optimizer=None):
     """Load a model's weights from a saved file with device compatibility."""
     # Map to the desired device (CPU or GPU)
     state_dict = torch.load(model_path, map_location=device)
+    model.load_state_dict(state_dict["model_state_dict"])
     if optimizer is not None:
-        optimizer.load_state_dict(state_dict["optimizer_state_dict"])
-        model.load_state_dict(state_dict["model_state_dict"])
         start_epoch = state_dict["epoch"]
+        optimizer.load_state_dict(state_dict["optimizer_state_dict"])
     else:
-        model.load_state_dict(state_dict)
+        # model.load_state_dict(state_dict)
         start_epoch = 1
         
     model.eval()  # Set to evaluation mode
