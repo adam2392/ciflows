a0fd26e54dcbdd6fead6da44ee40555f87596a7c
Adam Li <adam2392@gmail.com>
2024-12-27 13:01:37 -0500

    Try FFF vae
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- ciflows/flows/freeform.py
+++ ciflows/flows/freeform.py
@@ -4,6 +4,7 @@ from ciflows.distributions.pgm import LinearGaussianDag
 from ciflows.reduction.resnetvae import DeepResNetDecoder, ResNetEncoder
 from ciflows.loss import volume_change_surrogate
 
+
 class ResnetFreeformflow(nn.Module):
     def __init__(
         self,
@@ -34,17 +35,14 @@ class ResnetFreeformflow(nn.Module):
         embed_dim = self.latent_dim
         v_hat = v_hat.view(-1, embed_dim)
         loss_nll = (
-            -self.latent
-            .log_prob(v_hat, distr_idx=distr_idx.cpu())
-            - surrogate_loss
+            -self.latent.log_prob(v_hat, distr_idx=distr_idx.cpu()) - surrogate_loss
         ).mean()
-
 
         # compute the log-likelihood of the data
         # distr_idx = distr_idx.cpu()
         # log_prob, log_means, log_vars = self.latent.log_prob(z, distr_idx=distr_idx, return_means_log_vars=True)
 
-        return recon_x, surrogate_loss, loss_nll #log_prob, log_means, log_vars
+        return recon_x, surrogate_loss, loss_nll  # log_prob, log_means, log_vars
 
     def encode(self, x):
         return self.encoder(x)
