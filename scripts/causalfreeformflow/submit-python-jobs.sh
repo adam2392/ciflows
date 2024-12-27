f4c48a62754cb9efaa96f05da8b4b69df5ba8086
Adam Li <adam2392@gmail.com>
2024-12-27 13:34:11 -0500

    Try FFF vae
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- scripts/causalfreeformflow/submit-python-jobs.sh
+++ scripts/causalfreeformflow/submit-python-jobs.sh
@@ -38,7 +38,7 @@ LOG_FILE="celeba_fff_resnet_batch512_gradaccum_latentdim48_beta10_v1_${SCRIPT_NA
 # Optionally, you can use a job scheduler like `nohup` to run the command in the background
 # or `&` to run the command in the background
 export CUDA_VISIBLE_DEVICES=$GPU_INDICES
-CMD="torchrun --master_port=29501 --nproc_per_node=$NUM_GPUS $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
+CMD="torchrun --master_port=29502 --nproc_per_node=$NUM_GPUS $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
 LOG_FILE="celeba_vae_fff_resnet_batch128_gradaccum_latentdim48_beta1000_v1_${SCRIPT_NAME}_multigpu.log"
 nohup $CMD > $LOG_FILE 2>&1 &
 
