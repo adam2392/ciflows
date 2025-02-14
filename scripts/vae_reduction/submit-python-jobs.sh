09343c737d32a63d2df5dd72ad562230f4e17313
Adam Li <adam2392@gmail.com>
2025-02-13 22:05:26 -0500

    Try again
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- scripts/vae_reduction/submit-python-jobs.sh
+++ scripts/vae_reduction/submit-python-jobs.sh
@@ -3,6 +3,7 @@
 # Directory containing the Python script
 # SCRIPT_NAME="01_train_vae_celeba.py"
 SCRIPT_NAME="01_train_vae_ddp_celeba.py"
+SCRIPT_NAME="01_train_vae_ddp_mnist.py"
 SCRIPT_NAME="02_encode_celeba.py"
 # SCRIPT_NAME="03_train_nf_ddp_celeba.py"
 SCRIPT_NAME="03_train_nf_ddp_celeba.py"
@@ -13,7 +14,7 @@ LOG_DIR="/home/adam2392/projects/logs/"
 # cd "$SCRIPT_DIR"
 
 # Calculate the GPU index to use for this job
-GPU_INDEX=1
+GPU_INDEX=0
 
 # Specify the GPUs to use
 GPU_INDICES="0,1"  # Adjust this as per available GPUs and your requirement
@@ -28,7 +29,7 @@ CMD="python3 $SCRIPT_NAME" # --seed $TRAINING_SEED --log_dir $LOG_DIR"
 
 # Optionally, you can use a job scheduler like `nohup` to run the command in the background
 # or `&` to run the command in the background
-LOG_FILE="celeba_haircolor_innfon_128flows_alldata_cyclicresnetvaereduction_batch1024_latentdim48_hcdim8_trainableedges_sep4and8_v1_${SCRIPT_NAME}_seed_${GPU_INDEX}.log"
+LOG_FILE="mnist_cyclicbetal1loss_vaeresnetreduction_batch128_gradaccum_latentdim48_img64_v1_${SCRIPT_NAME}_seed_${GPU_INDEX}.log"
 # LOG_FILE="celeba_cyclicbetal1loss_haircolorscm_vaeresnetreduction_batch128_gradaccum_latentdim48_img128_v1_${SCRIPT_NAME}_seed_${GPU_INDEX}.log"
 # LOG_FILE="encodings_haircolor_celeba_alldata_cyclicbeta_noimageaug_vaeresnetreduction_batch1024_norm01_latentdim48_img128_v1cont_${SCRIPT_NAME}_seed_${GPU_INDEX}.log"
 
